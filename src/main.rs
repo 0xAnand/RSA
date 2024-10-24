03e91351c3ea112d0256f864a37aa1cc1df3cba5
0xAnand <anandvtiwari.mail@gmail.com>
2024-10-24 13:52:26 +0530

    Testing gitignore

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 //public-key encryption algorithm enables secure data transmission, encrypting it with public key and decrypting it with private key
 
+fn caesar_cypher(msg: String, cShift: u8) -> String {
+    let encrypted_msg = "";
+    for c in msg.chars() {
+        let temp = c as u8;
+        temp = temp + cShift;
+        encrypted_msg = encrypted_msg + (temp as &str);
+    }
+
+    return encrypted_msg;
+}
 
 fn main() {
+    let msg = String::from("Hello there");
+    let encrypted_msg = caesar_cypher(msg,1);
+    println!("{}", encrypted_msg);
 
 }
